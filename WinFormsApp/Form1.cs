4fbee284083ca36a99e2c0662dc297185af0d2d2
Ichi Kuandoru <mister.nik2002@yandex.ru>
2024-03-16 18:45:02 +0700

    Добавьте файлы проекта.

---
+++ WinFormsApp/Form1.cs
@@ -0,0 +1,36 @@
+using System.Xml.Linq;
+
+namespace WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //получаем значение текстового пол€ + отрезаем пробелы по кра€м:
+            string userName = textBox1.Text.Trim();
+            if (userName.Length < 3) //если длина имени пользовател€ слишком коротка€, ошибка!
+            {
+                MessageBox.Show("¬ведите им€ пользовател€!");
+            }
+            else
+            {
+                MessageBox.Show("ѕривет " + userName);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WinFormsApp/Form2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+Color DefaultColor
+{
+    get { return Color.White; }
+}
+
+void CreateBlank(int width, int height)
+{
+    var OldImage = pictureBox1.Image;
+
+    var bmp = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            bmp.SetPixel(i, j, DefaultColor);
+        }
+    }
+    pictureBox1.Image = bmp;
+    if (OldImage != null)
+    {
+        OldImage.Dispose();
+    }
+}
+
+namespace WinFormsApp
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
