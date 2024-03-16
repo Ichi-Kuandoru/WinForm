cdc5fc26884c886f7b1bb9d10b693cb53aac663f
Ichi Kuandoru <mister.nik2002@yandex.ru>
2024-03-16 19:05:30 +0700

    Был создан DefaultColor и CreateBlank

--- WinFormsApp/Form2.cs
+++ WinFormsApp/Form2.cs
@@ -9,35 +9,36 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-Color DefaultColor
-{
-    get { return Color.White; }
-}
 
-void CreateBlank(int width, int height)
-{
-    var OldImage = pictureBox1.Image;
-
-    var bmp = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
-
-    for (int i = 0; i < width; i++)
-    {
-        for (int j = 0; j < height; j++)
-        {
-            bmp.SetPixel(i, j, DefaultColor);
-        }
-    }
-    pictureBox1.Image = bmp;
-    if (OldImage != null)
-    {
-        OldImage.Dispose();
-    }
-}
 
 namespace WinFormsApp
 {
     public partial class Form2 : Form
     {
+        Color DefaultColor
+        {
+            get { return Color.White; }
+        }
+
+        void CreateBlank(int width, int height)
+        {
+            var OldImage = pictureBox1.Image;
+
+            var bmp = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    bmp.SetPixel(i, j, DefaultColor);
+                }
+            }
+            pictureBox1.Image = bmp;
+            if (OldImage != null)
+            {
+                OldImage.Dispose();
+            }
+        }
         public Form2()
         {
             InitializeComponent();
